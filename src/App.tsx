d4512f56b828e64e704060a1175145fe98202d1e
Rapha Skywalker <raphael@mandalorian.com>
2026-04-02 15:05:09 -0300

    fix: revert base path for nodejs domain deployment

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.DEV ? "/" : "./"}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/apresentacao/carta-presidente" element={<CartaPresidente />} />
